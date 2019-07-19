890ba880988d5a681b6eefcc6aeb98967ce03ad6
Sangsaka Wira <sangsakawira@gmail.com>
2019-07-19 07:30:08 +0700

    sensor sudah sip

--- routes/laporan.js
+++ routes/laporan.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+// create
+// getAll
+// getById/:id
+// updateById/:id
+// deleteById/:id
+
 let urlencodedParser = bodyParser.urlencoded({
     extended: false
 })
@@ -8,13 +14,20 @@ const router = express.Router()
 
 const laporanController = require("../controllers/laporan")
 
+// READ
 router.get("/getAll",laporanController.getAll)
 router.get("/getById/:id",laporanController.getById) 
-router.get("/getByUserId/:id",laporanController.getByUserId) //get laporan by user id
+router.get("/getByUserId/:id",laporanController.getByUserId) 
 router.get("/getByUserName/:name",laporanController.getByUserName)
 router.get("/getByPemilik",laporanController.getByPemilik)
+
+// UPDATE
 router.patch("/updateById/:id",urlencodedParser,laporanController.updateById)
+
+// CREATE
 router.post("/create",urlencodedParser,laporanController.register)
+
+// DELETE
 router.delete("/delete/:id",laporanController.deleteById)
 
 module.exports = router
